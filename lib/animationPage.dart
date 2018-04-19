ed324c972113b50e7ba022d845d81447d008a2a7
zzy <1097088392@qq.com>
2018-04-19 17:56:20 +0800

    animation page

--- lib/animationPage.dart
+++ lib/animationPage.dart
@@ -15,10 +15,20 @@ class AnimationStateful extends StatefulWidget{
   createState() => new AnimationState();
 }
 
+// SingleTickerProviderStateMixin
+
 class AnimationState extends State<AnimationStateful> with TickerProviderStateMixin{
   double imgOpacity = 1.0;
   AnimationController ac;
   double initValue = 0.0;
+
+  AnimationController ac2;
+  Animation a2;
+  Animation a3;
+  Animation reverseAnimation;
+  Animation a4;
+  Animation a5;
+  
 
   @override
   void initState(){
@@ -32,11 +42,23 @@ class AnimationState extends State<AnimationStateful> with TickerProviderStateMi
       });
     });
     // ac.forward();
+
+    ac2 = new AnimationController(duration: const Duration(seconds: 3), vsync: this);
+    a2 = new Tween(begin: 200.0,end: 300.0).animate(ac2);
+
+    final CurvedAnimation ca = new CurvedAnimation(parent: ac2, curve: Curves.easeIn);
+    a3 = new Tween(begin: 200.0,end: 300.0).animate(ca);
+
+    reverseAnimation = new ReverseAnimation(ac2);
+    a4 = new Tween(begin: 200.0,end: 300.0).animate(reverseAnimation);
+
+    a5 = new ColorTween(begin: Colors.teal,end: Colors.yellow).animate(ac2);
   }
 
   @override
   void dispose() {
     ac.dispose();
+    ac2.dispose();
     super.dispose();
   }
 
@@ -80,6 +102,7 @@ class AnimationState extends State<AnimationStateful> with TickerProviderStateMi
               onPressed: (){
                 print('22');
                 ac.forward(from: 0.0);
+                ac2.forward(from: 0.0);
               },
             )
           ),
@@ -92,9 +115,67 @@ class AnimationState extends State<AnimationStateful> with TickerProviderStateMi
               color: Colors.teal,
               height: 50.0
             ) 
+          ),
+          new AnimatedRoundImage(animation: a2, fun: (){
+            ac2.forward(from: 0.0);
+          }),
+          new Center(
+            child: new AnimatedBuilder(
+              child: new Text('AnimatedBuilder'),
+              animation: a4,
+              builder: (BuildContext context, Widget child){
+                return new Container(
+                  margin: new EdgeInsets.symmetric(vertical: 100.0),
+                  color: a5.value,
+                  child: child,
+                  width: a4.value,
+                  height: a4.value,
+                );
+              },
+            )
           )
         ],
       ),
     );
+  }
+}
+
+class AnimatedRoundImage extends AnimatedWidget{
+  Function fun;
+  AnimatedRoundImage({Key key, Animation<double> animation, Function fun}):super(key:key, listenable: animation){
+    this.fun = fun;
+  }
+
+  @override
+  Widget build(BuildContext context){
+    final Animation<double> ani = listenable;
+    return new Center(
+      child: new GestureDetector(
+        onTap: (){
+          print('tap');
+          fun();
+        },
+        child: new Container(
+          decoration: new BoxDecoration(
+            color: Colors.yellow[600],
+            borderRadius: new BorderRadius.all(new Radius.circular(ani.value / 2)),
+            image: new DecorationImage(
+              image: new NetworkImage('http://p9.pstatp.com/large/9aa0003fa8213724981'),
+              fit: BoxFit.cover
+            ),
+          ),
+          /*foregroundDecoration: new BoxDecoration(
+            image: new DecorationImage(
+              image: new NetworkImage('http://p9.pstatp.com/large/9aa0003fa8213724981'),
+              fit: BoxFit.cover
+            ),
+            borderRadius: new BorderRadius.all(new Radius.circular(ani.value / 2))
+          ),*/
+          //child: new Image.network('http://img2.178.com/acg1/201312/181193085837/181196147567.jpg'),
+          width: ani.value,
+          height: ani.value,
+        )
+      ), 
+    ) ;
   }
 }
